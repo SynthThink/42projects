548970325f728f1894eb7667de209a7580dc7328
Maxence ALLUIN <malluin@e2r2p3.42.fr>
2019-02-05 18:43:24 +0100

    delete

--- fillit/samples/test.sh
+++
@@ -1,8 +0,0 @@
-../fillit invalid_sample1.fillit
-../fillit invalid_sample2.fillit
-../fillit invalid_sample3.fillit
-../fillit invalid_sample4.fillit
-../fillit invalid_sample5.fillit
-../fillit invalid_sample6.fillit
-../fillit too_long_sample.fillit
-../fillit valid_sample.fillit
